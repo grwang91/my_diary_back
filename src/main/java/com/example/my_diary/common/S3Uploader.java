ba0dc92fdca2d76ca2793d6cc3dab9c4408a33e6
grwang <53140990+grwang91@users.noreply.github.com>
2020-08-29 01:22:17 +0900

    aws credential 수정

--- src/main/java/com/example/my_diary/common/S3Uploader.java
+++ src/main/java/com/example/my_diary/common/S3Uploader.java
@@ -1,8 +1,6 @@
 package com.example.my_diary.common;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.*;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
@@ -27,11 +25,11 @@ public class S3Uploader {
     private AmazonS3 s3Client;
 
 
-    @Value("${cloud.aws.credentials.accessKey}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secretKey}")
-    private String secretKey;
+//    @Value("${cloud.aws.credentials.accessKey}")
+//    private String accessKey;
+//
+//    @Value("${cloud.aws.credentials.secretKey}")
+//    private String secretKey;
 
     @Value("${cloud.aws.region.static}")
     private String region;
@@ -41,10 +39,16 @@ public class S3Uploader {
 
     @PostConstruct
     public void setS3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+
+        //AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+
+//        s3Client = AmazonS3ClientBuilder.standard()
+//                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+//                .withRegion(this.region)
+//                .build();
 
         s3Client = AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withCredentials(new InstanceProfileCredentialsProvider(false))
                 .withRegion(this.region)
                 .build();
     }
